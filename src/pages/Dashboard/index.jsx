a878e092d9456610e662b6025fa56480d454b59a
Arnaud Balland <balland.arnaud@gmail.com>
2022-09-28 18:26:01 +0200

    Add nutrient section

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -2,9 +2,14 @@ import Mock from '../../__mocks__/mock'
 import Greeting from '../../components/Greeting'
 import DailyActivityChart from '../../components/DailyActivityChart'
 import AverageSessionsChart from '../../components/AverageSessionsChart'
-import './index.css'
 import PerformanceChart from '../../components/PerformanceChart'
 import GoalChart from '../../components/GoalChart'
+import iconCalories from '../../assets/svg/icon-calories.svg'
+import iconProteins from '../../assets/svg/icon-proteins.svg'
+import iconCarbohydrates from '../../assets/svg/icon-carbohydrates.svg'
+import iconLipids from '../../assets/svg/icon-lipids.svg'
+import './index.css'
+import mock from '../../__mocks__/mock'
 
 function Dashboard() {
   return (
@@ -26,7 +31,52 @@ function Dashboard() {
             <GoalChart data={Mock.USER_MAIN_DATA[0]} />
           </div>
         </div>
-        <div className="dashboard__nutrients"></div>
+        <div className="dashboard__nutrients">
+          <div className="dashboard__nutrient">
+            <div className="dashboard__nutrientIcon dashboard__nutrientIcon--calories">
+              <img src={iconCalories} alt="" />
+            </div>
+            <div className="dashboard__nutrientInformations">
+              <p className="dashboard__nutrientQuantity">
+                {mock.USER_MAIN_DATA[0].keyData.calorieCount}Cal
+              </p>
+              <p className="dashboard__nutrientName">Calories</p>
+            </div>
+          </div>
+          <div className="dashboard__nutrient">
+            <div className="dashboard__nutrientIcon dashboard__nutrientIcon--proteins">
+              <img src={iconProteins} alt="" />
+            </div>
+            <div className="dashboard__nutrientInformations">
+              <p className="dashboard__nutrientQuantity">
+                {mock.USER_MAIN_DATA[0].keyData.proteinCount}g
+              </p>
+              <p className="dashboard__nutrientName">Proteines</p>
+            </div>
+          </div>
+          <div className="dashboard__nutrient">
+            <div className="dashboard__nutrientIcon dashboard__nutrientIcon--carbohydrates">
+              <img src={iconCarbohydrates} alt="" />
+            </div>
+            <div className="dashboard__nutrientInformations">
+              <p className="dashboard__nutrientQuantity">
+                {mock.USER_MAIN_DATA[0].keyData.carbohydrateCount}g
+              </p>
+              <p className="dashboard__nutrientName">Glucides</p>
+            </div>
+          </div>
+          <div className="dashboard__nutrient">
+            <div className="dashboard__nutrientIcon dashboard__nutrientIcon--lipids">
+              <img src={iconLipids} alt="" />
+            </div>
+            <div className="dashboard__nutrientInformations">
+              <p className="dashboard__nutrientQuantity">
+                {mock.USER_MAIN_DATA[0].keyData.lipidCount}g
+              </p>
+              <p className="dashboard__nutrientName">Lipides</p>
+            </div>
+          </div>
+        </div>
       </div>
     </main>
   )
